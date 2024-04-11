a1372a1def6c5b07e51826c7a30f78035f30dc4d
jackkilkenny10 <G00401535@atu.ie>
2024-04-11 12:27:51 +0100

    new feature add

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,8 +1,21 @@
 package ie.atu;
 
 public class Calc {
-    public int add(int firstNum, int secondNum){
-        int result =firstNum + secondNum;
+    public int add(int firstNum, int secondNum) {
+        int result = firstNum + secondNum;
         return result;
     }
+    public int Sub(int firstNum, int secondNum){
+        int result2 = firstNum - secondNum;
+        return result2;
+    }
+    public int Div(int firstNum, int secondNum){
+        int result3 = firstNum / secondNum;
+        return result3;
+    }
+    public int Multi(int firstNum, int secondNum){
+        int result4 = firstNum * secondNum;
+        return result4;
+    }
 }
+
--- src/test/java/ie/atu/Calctest.java
+++ src/test/java/ie/atu/Calctest.java
@@ -6,13 +6,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Calctest {
     Calc newCalc;
-
+    Calc newCalc2;
+    Calc newCalc3;
+    Calc newCalc4;
 
     @Test
     void testAdd(){
        newCalc = new Calc();
-       assertEquals(10,newCalc.add(4,6));
+       assertEquals(10,newCalc.add(6,4));
 
     }
 
+    @Test
+    void testSub(){
+        newCalc2 = new Calc();
+        assertEquals(2,newCalc.Sub(6,4));
+
+    }
+
+    @Test
+    void testDiv(){
+        newCalc3 = new Calc();
+        assertEquals(5,newCalc.Div(10,2));
+
+    }
+
+    @Test
+    void testMulti(){
+        newCalc4 = new Calc();
+        assertEquals(12,newCalc.Multi(6,2));
+
+    }
 }
